202adccc5169e4f0d6f5fe63b9d07d10e02a29e5
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-03-30 00:59:01 -0400

    fix feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,41 +1,43 @@
 from rdkit import Chem
 
 def nist_id_to_rdkit_mol(myid):
-    path = '/home/gaoxiang/MEGA/shared/mol_files/{}.mol'
-    return Chem.MolFromMolFile(path.format(myid))
+	path = '/home/gaoxiang/MEGA/shared/mol_files/{}.mol'
+	return Chem.MolFromMolFile(path.format(myid))
 
 class extractor(object):
-	def __init__(self):
-		pass
+	def __init__(self,name):
+		self.name = name
 	def extract(self,myid):
 		pass
 
 class or_extractor(extractor):
-	def __init__(self,*extractors):
-		super.__init__()
+	def __init__(self,name,*extractors):
+		super(or_extractor,self).__init__(name)
 		self.extractors = extractors
 	def extract(self,myid):
 		inside = False
 		for i in self.extractors:
 			inside = inside or i.extract(myid)
+		return inside
 
 class and_extractor(extractor):
-	def __init__(self,*extractors):
-		super.__init__()
+	def __init__(self,name,*extractors):
+		super(and_extractor,self).__init__(name)
 		self.extractors = extractors
 	def extract(self,myid):
 		inside = True
 		for i in self.extractors:
 			inside = inside and i.extract(myid)
+		return inside
 
 class substruct_searcher(extractor):
-    def __init__(self,substructure):
-        super.__init__()
-        self.substructure = substructure
-    def extract(self,myid):
-        m = nist_id_to_rdkit_mol(myid)
-        return m.HasSubstructMatch(self.substructure)
+	def __init__(self,name,substructure):
+		super(substruct_searcher,self).__init__(name)
+		self.substructure = substructure
+	def extract(self,myid):
+		m = nist_id_to_rdkit_mol(myid)
+		return m.HasSubstructMatch(self.substructure)
 
 class smiles_searcher(substruct_searcher):
-    def __init__(self,smiles):
-        super.__init__(Chem.MolFromSmiles(smiles))
+	def __init__(self,name,smiles):
+		super(smiles_searcher,self).__init__(name,Chem.MolFromSmiles(smiles))
