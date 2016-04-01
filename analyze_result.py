7aa5b9e977ae33bbf167c6a2caa157d290bb6041
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-04-01 15:00:51 -0400

    support different net

--- analyze_result.py
+++ analyze_result.py
@@ -6,17 +6,18 @@ overall_accuracy_arr = []
 paccuracy_arr = []
 naccuracy_arr = []
 label=open('label').readline().strip()
+net=open('net').readline().strip()
 
 for i in range(3):
-	net = 'irnet-{}.prototxt'.format(i)
-	weight = '{}_iter_35000.caffemodel'.format(i)
+	netfn = '{}-{}.prototxt'.format(net,i)
+	weightfn = '{}_{}_iter_35000.caffemodel'.format(net,i)
 	testdbfn = 'data-{}.h5'.format(i)
-	overall_accuracy,paccuracy,naccuracy = run_test(net,weight,testdbfn,label)
+	overall_accuracy,paccuracy,naccuracy = run_test(netfn,weightfn,testdbfn,label)
 	print 'cross validation:',i,'\toverall accuracy:',overall_accuracy,'\t++ rate:', paccuracy,'\t-- rate:', naccuracy
 	overall_accuracy_arr.append(overall_accuracy)
 	paccuracy_arr.append(paccuracy)
 	naccuracy_arr.append(naccuracy)
-	
+
 print '\nSummary:'
 print 'overall accuracy:',sum(overall_accuracy_arr)/len(overall_accuracy_arr)
 print '++ rate:',sum(paccuracy_arr)/len(paccuracy_arr)
--- train.sh
+++ train.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 label=$(cat label)
+net=$(cat net)
 
 #generate network and data file list for cross validation
 for cv in {0..2};do
-	sed "s/<cv>/$cv/g" solver.prototxt > solver-$cv.prototxt
-	sed "s/<label>/$label/g;s/<cv>/$cv/g" irnet.prototxt > irnet-$cv.prototxt
+	sed "s/<cv>/$cv/g" $net-solver.prototxt > $net-solver-$cv.prototxt
+	sed "s/<label>/$label/g;s/<cv>/$cv/g" $net.prototxt > $net-$cv.prototxt
 	echo "data-$cv.h5" > test-$cv.txt
 	echo data-{0..2}.h5|sed 's/ /\n/g'|grep -v $cv > train-$cv.txt
 done
@@ -13,7 +14,7 @@ done
 #train the three net
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-7.0/lib64:/apps/caffe/build/lib
 for cv in {0..2};do
-	/apps/caffe/build/tools/caffe train --solver=solver-$cv.prototxt 2>&1|tee train-$cv.log
+	/apps/caffe/build/tools/caffe train --solver=$net-solver-$cv.prototxt 2>&1|tee $net-train-$cv.log
 done
 
 #analyze the result
