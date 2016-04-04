df244c408387aa60d3d1c7138c91227f2ca33e0c
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-04-04 15:57:03 -0400

    clean pyc

--- clean.sh
+++ clean.sh
@@ -2,6 +2,7 @@
 
 rm -f *.binaryproto *.npy *.h5
 rm -f test-[0-9].txt train-[0-9].txt
+rm -f *.pyc
 rm -f lenet-[0-9].prototxt lenet-solver-[0-9].prototxt
 rm -f cifar10-[0-9].prototxt cifar10-solver-[0-9].prototxt
 rm -f *.log
