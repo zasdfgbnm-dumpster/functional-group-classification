64d807037f7c16d1b90e7f8e9615a825a39e7137
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-04-04 15:42:08 -0400

    add script to do clean

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f *.binaryproto *.npy *.h5
+rm -f test-[0-9].txt train-[0-9].txt
+rm -f lenet-[0-9].prototxt lenet-solver-[0-9].prototxt
+rm -f cifar10-[0-9].prototxt cifar10-solver-[0-9].prototxt
+rm -f *.log
+rm -f train.[oe]*
